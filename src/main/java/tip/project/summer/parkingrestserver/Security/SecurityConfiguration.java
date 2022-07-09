74566bb5236fe4a7a656420b4ace0acd3d87699b
zarasuto <zarasuto@gmail.com>
2022-07-10 04:02:55 +0800

    Fixed Authentication not getting HttpRequests

--- src/main/java/tip/project/summer/parkingrestserver/Security/SecurityConfiguration.java
+++ src/main/java/tip/project/summer/parkingrestserver/Security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package tip.project.summer.parkingrestserver.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import tip.project.summer.parkingrestserver.jwt.JwtUsernameAndPasswordAuthentica
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
