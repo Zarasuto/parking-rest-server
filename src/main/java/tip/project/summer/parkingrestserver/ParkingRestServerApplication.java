12ba11b8f31e1aaca0ef5607d814b38db7e70a19
zarasuto <zarasuto@gmail.com>
2022-07-15 00:16:51 +0800

    Revert "added message for testing added Azure cloud apps"
    This reverts commit 003d22fe

--- src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
+++ src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
@@ -2,16 +2,20 @@ package tip.project.summer.parkingrestserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ParkingRestServerApplication{
+@RestController
+public class ParkingRestServerApplication {
 
+    @GetMapping("/message")
+    public String start(){
+        return "REST Parking Server";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingRestServerApplication.class, args);
     }
+
 }
