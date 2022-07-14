003d22fe261573a0f8ff92f640ae80a46187676a
zarasuto <zarasuto@gmail.com>
2022-07-14 14:16:57 +0800

    added message for testing added Azure cloud apps

--- src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
+++ src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
@@ -2,9 +2,17 @@ package tip.project.summer.parkingrestserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingRestServerApplication {
+
+    @GetMapping("/message")
+    public String start(){
+        return "REST Parking Server";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingRestServerApplication.class, args);
