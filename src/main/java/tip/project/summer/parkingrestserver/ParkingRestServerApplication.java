f01dc837ef376d890f9039910e4e107465f06370
zarasuto <zarasuto@gmail.com>
2022-07-14 18:37:06 +0800

    changed back to jar, removed GetMapping in main function

--- src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
+++ src/main/java/tip/project/summer/parkingrestserver/ParkingRestServerApplication.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ParkingRestServerApplication {
-
-    @GetMapping("/")
-    public String start(){
-        return "REST Parking Server";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingRestServerApplication.class, args);
